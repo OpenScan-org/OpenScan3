2b667ebd0088012faf07bf44bb0389cc0f069396
polectron <polectron@gmail.com>
2022-08-06 19:12:58 +0100

    update cloud controller

--- app/controllers/cloud.py
+++ app/controllers/cloud.py
@@ -28,7 +28,7 @@ def _cloud_request(method: str, path: str, params=None) -> requests.Response:
 
 
 def get_token_info() -> dict[str, Any]:
-    return _cloud_request("get", "getTokenInfo").json()
+    return _cloud_request("get", "getTokenInfo")
 
 
 def _create_project(
@@ -43,7 +43,7 @@ def _create_project(
             "parts": parts,
             "project": project_name,
         },
-    ).json()
+    )
 
 
 def _upload_file(file: IO[bytes], ulink: str):
@@ -54,9 +54,7 @@ def _upload_file(file: IO[bytes], ulink: str):
 
 def upload_project(project_name: str):
     project = projects.get_project(project_name)
-    photos = projects.get_project_photos(project)
-
-    cloud_project_name = f"{project.name}_{int(time.time())}"
+    photos = project.photos
 
     # compress
     zip = projects.compress_project_photos(project)
@@ -64,7 +62,7 @@ def upload_project(project_name: str):
     # split
     nchunks = math.ceil(zip_size / config.cloud.split_size)
     # create project
-    response = _create_project(cloud_project_name, len(photos), zip_size, nchunks)
+    response = _create_project(project.name, len(photos), zip_size, nchunks)
 
     if response.status_code == 200:
         info = response.json()
@@ -76,11 +74,11 @@ def upload_project(project_name: str):
             chunk.close()
             counter += 1
         # start processing
-        response3 = _start_project(cloud_project_name)
+        response3 = _start_project(project.name)
         print(response3)
         print(response3.text)
 
-        print(get_project_info(cloud_project_name).json())
+        print(get_project_info(project.name).json())
 
     zip.close()
 
@@ -88,10 +86,10 @@ def upload_project(project_name: str):
 def _start_project(project_name: str) -> dict[str, Any]:
     return _cloud_request(
         "get", "startProject", params={"project": project_name}
-    ).json()
+    )
 
 
 def get_project_info(project_name: str) -> dict[str, Any]:
     return _cloud_request(
         "get", "getProjectInfo", params={"project": project_name}
-    ).json()
+    )
