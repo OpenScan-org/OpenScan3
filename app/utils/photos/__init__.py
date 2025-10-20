7318c6af80ac4a610c029c8b38a68cf9be60ef8f
Thomas <info@openscan.eu>
2025-10-20 18:41:24 +0200

    added FocusStacker utility
    - automatically detects batch size
    - allows standalone calibration
    - TODO:
        - update scan metadata with calibration results
        - add resumability

--- app/utils/photos/__init__.py
+++
@@ -1,4 +0,0 @@
-from .heatmap import calculate_heatmap, apply_heatmap
-from .histogram import calculate_histogram, apply_histogram
-
-__all__ = ['calculate_heatmap', 'apply_heatmap', 'calculate_histogram', 'apply_histogram']
